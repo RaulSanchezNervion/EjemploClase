15382920ad624420b7f7fb8de8e7a7515e3b0e69
Raul Sanchez <raul.sanchez@iesnervion.es>
2020-11-25 19:12:54 +0100

    asd

---
+++ src/asdasd/asdasd.java
@@ -0,0 +1,40 @@
+package asdasd;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+
+public class asdasd extends Composite {
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public asdasd(Composite parent, int style) {
+		super(parent, style);
+		
+		Button btnCheckButton = new Button(this, SWT.CHECK);
+		btnCheckButton.setBounds(59, 65, 111, 20);
+		btnCheckButton.setText("Check Button");
+		
+		Button btnCheckButton_1 = new Button(this, SWT.CHECK);
+		btnCheckButton_1.setBounds(59, 111, 111, 20);
+		btnCheckButton_1.setText("Check Button");
+		
+		Button btnCheckButton_2 = new Button(this, SWT.CHECK);
+		btnCheckButton_2.setBounds(59, 165, 111, 20);
+		btnCheckButton_2.setText("Check Button");
+		
+		Label lblPreguntaTipoTest = new Label(this, SWT.NONE);
+		lblPreguntaTipoTest.setBounds(37, 22, 182, 20);
+		lblPreguntaTipoTest.setText("Pregunta tipo test");
+
+	}
+
+	@Override
+	protected void checkSubclass() {
+		// Disable the check that prevents subclassing of SWT components
+	}
+}
