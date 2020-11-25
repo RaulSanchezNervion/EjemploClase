ed844c2e5cdc7531504b77f2278246d209e62b4c
RaulSanchezNervion <raul.sanchez@iesnervion.es>
2020-11-25 20:00:31 +0100

    sdfsdf

--- src/asdasd/asdasd.java
+++ src/asdasd/asdasd.java
@@ -29,7 +29,7 @@ public class asdasd extends Composite {
 		
 		Label lblPreguntaTipoTest = new Label(this, SWT.NONE);
 		lblPreguntaTipoTest.setBounds(37, 22, 182, 20);
-		lblPreguntaTipoTest.setText("Pregunta tipo testkkk y");
+		lblPreguntaTipoTest.setText("Question");
 
 	}
 
